dfa85a975626d31a04be2893d97603af8a9d111e
AdrianFermanelli <104604865+AdrianFermanelli@users.noreply.github.com>
2022-12-15 10:10:17 -0300

    js work

---
+++ Js/ticket.js
@@ -0,0 +1,31 @@
+function resumen() {
+    let cantidad = parseInt(document.getElementById("cantidadTicket").value);
+    let categoria = document.getElementById("inputGroupSelect01").value;
+    console.log(categoria);
+    let total = document.getElementById("TotalPagar");
+    console.log("INICIO")
+    let num;
+    num = cantidad * 200;
+    if (categoria == "1") {
+        num = num - ((num * 80) / 100);
+    } else if (categoria == "2") {
+        num = num - ((num * 50) / 100);
+    } else {
+        num = num - ((num * 15) / 100);
+    }
+
+    total.value = "Total a Pagar: $" + num;
+}
+
+function borrar() {
+    let cantidad = document.getElementById("cantidadTicket");
+    cantidad = "";
+    let total = document.getElementById("TotalPagar");
+    total = "Total a Pagar: $";
+    let nombre = document.getElementById("nombre");
+    nombre = "";
+    let apellido = document.getElementById("apellido");
+    apellido = "";
+    let email = document.getElementById("email");
+    email = "";
+}
